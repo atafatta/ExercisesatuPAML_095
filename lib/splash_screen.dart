b446e756d03dc108edc433f06d26c7a60bd7565c
atafatta <atafatta@gmail.com>
2023-03-30 23:50:44 +0700

    navigasi button Get Started "Splash Screen" ke "Login Page

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:exercise_satu/login_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -29,23 +30,28 @@ class SplashScreen extends StatelessWidget {
             Container(
               height: 50.0,
               width: double.infinity,
-              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              margin: EdgeInsets.symmetric(horizontal: 50.0),
               child: ElevatedButton(
                 onPressed: () {
                   // Add your onPressed function here
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const loginpage()));
                 },
                 child: Text(
                   'Get Started',
                   style: TextStyle(
                     fontSize: 20.0,
+                    fontFamily: 'Montserrat',
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
                 ),
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
+                  primary: Color.fromARGB(255, 255, 230, 0),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
                 ),
               ),
